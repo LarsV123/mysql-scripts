7c7441ac1d1cec1c319b14c777d5879a068c0aed
LarsV123 <42863501+LarsV123@users.noreply.github.com>
2023-03-02 13:49:31 +0100

    feat: Add build script for release version

---
+++ build-release.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Script for building MySQL such that it can be run with
+# ./release-build/runtime_output_directory/mysqld --datadir=./data --initialize-insecure
+
+# This script assumes that you have a folder ~/mysql/boost
+# with the boost libraries in it.
+BUILD_DIR=~/mysql/release-build
+DATA_DIR=~/mysql/data
+
+# Exit on error
+set -e
+
+# Create build folder (no error if it already exists)
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+echo "Building MySQL in folder: $PWD"
+
+echo "Running cmake"
+cmake ../mysql-server/ \
+  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+  -DWITH_SYSTEM_LIBS=0 \
+  -DWITH_UNIT_TESTS=0 \
+  -DWITH_ROUTER=0 \
+  -DWITH_AUTHENTICATION_FIDO=0 \
+  -DWITH_NDB=0 \
+  -DWITH_ZLIB=bundled \
+  -DWITH_BOOST=~/mysql/boost \
+  -DWITH_ASAN=0 \
+  -DWITH_NDBCLUSTER_STORAGE_ENGINE=0 \
+  -DWITH_ICU=system \
+  -GNinja
+
+# -DCMAKE_PREFIX_PATH=/usr/include/ \
+echo "Running ninja"
+# Use -j4 to build with 4 threads, more threads require more RAM (> 16 GB)
+ninja -j4
+
+echo "Done building MySQL"
+
+INIT_SERVER="${BUILD_DIR}/runtime_output_directory/mysqld --datadir=${DATA_DIR} --initialize-insecure"
+RUN_SERVER="${BUILD_DIR}/runtime_output_directory/mysqld --datadir=${DATA_DIR}"
+RUN_CLIENT="${BUILD_DIR}/runtime_output_directory/mysql -uroot"
+
+printf "\n**********\n"
+printf "Initialize server:\n  ${INIT_SERVER}\n"
+printf "Run server:\n  ${RUN_SERVER}\n"
+printf "Run client:\n  ${RUN_CLIENT}\n"
